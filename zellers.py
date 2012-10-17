e20bc46479fdf5104b8d9e968f4faf7959666f55
Fabien <fabien@lapin-blanc.com>
2012-10-17 11:29:51 +0200

    Several checks implemented

--- zellers.py
+++ zellers.py
@@ -1,4 +1,5 @@
-# First draw for zellers optionnal homework
+# Zellers optionnal homework
+# 
 
 months = ["march",
            "april",
@@ -22,18 +23,46 @@ days = ["sunday",
         "saturday",
         "sunday"]
 
-month_name = raw_input("Month: ").lower()
+# Input a year of the gregorian calendar
+# loop while input is invalid
+year = input("Year: ")
+while year<1582:
+    year = input("Enter a year of the gregorian calendar (>=1582): ")
+C = year % 100              # C assigned to year : 89 for 1989, ...
+D = year // 100             # A assigned to century : 19 for 1989, ...
+
+# Input a month name (ie: january, february, etc.
+# loop while input is invalid
+month_name = raw_input("Month name in english: ").lower()
 while month_name not in months:
     month_name = raw_input("Please enter a valid month name: ")
 A = months.index(month_name)+1
-# TODO check day value
+month_num = (A + 1) % 12    # Jan = 0, Feb = 1, ...
+# A contains index of month for algorithm
+# month_num contains month index as with Jan = 0, Feb = 1, ...
+
+
+# Input a day number
+# loop while input is invalid
+# Leap year ?
+is_leap = (year%4 == 0) and not ((year%100 == 0) and (year%400 != 0))
+# compute number of days in the choosen month
+if month_num == 1:          # February
+    if is_leap:
+        month_days = 29
+    else:
+        month_days = 28
+else:
+    month_days = 31 - (month_num % 7 % 2)
 B = input("Day: ")
-# TODO check year value
-C = input("Year: ")
+while not((B>0) and (B<=month_days)):
+    B = input("Please input a valid day number for this month: ")
+
+
+
+# Corrections for algorithm
 if A in [11,12]:
     C -= 1
-# TODO check year value
-D = input("Century: ")
 if C == -1:
     D -= 1
     C = 99
@@ -45,4 +74,4 @@ Y = D / 4
 Z = W + X + Y + B + C - 2*D
 R = Z%7
 
-print days[R], month_name, B, str(D)+str(C)
+print days[R], month_name, B, year
